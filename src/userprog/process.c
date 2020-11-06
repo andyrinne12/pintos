4eb26e92164825bf404ce970082e8617ecbc1fe5
Vlad <vladnicolaescucollege@gmail.com>
2020-11-06 19:52:10 +0200

    [vn119] Update on argument passing functionality. Add fake set-up for minimal stack

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -26,22 +26,31 @@ static bool load (const char *cmdline, void (**eip) (void), void **esp);
    before process_execute() returns.  Returns the new process's
    thread id, or TID_ERROR if the thread cannot be created. */
 tid_t
-process_execute (const char *file_name)
+process_execute (const char *command_line)
 {
-  char *fn_copy;
+  char *cmd_line_copy;
+
+  /* FILE_NAME keeps track of the file name (first token in command line) */
+  char *file_name;
+  /* ARGUMENTS keeps track of the program arguments (remaining tokens) */
+  char *arguments;
+
   tid_t tid;
 
-  /* Make a copy of FILE_NAME.
+  /* Make a copy of COMMAND_LINE.
      Otherwise there's a race between the caller and load(). */
-  fn_copy = palloc_get_page (0);
-  if (fn_copy == NULL)
+  cmd_line_copy = palloc_get_page (0);
+  if (cmd_line_copy == NULL)
 	return TID_ERROR;
-  strlcpy (fn_copy, file_name, PGSIZE);
+  strlcpy (cmd_line_copy, command_line, PGSIZE);
+
+  /* Tokenize the command line and recognize the first as the FILE_NAME */
+  file_name = strtok_r((char *) command_line, " ", &arguments);
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (file_name, PRI_DEFAULT, start_process, cmd_line_copy);
   if (tid == TID_ERROR)
-	palloc_free_page (fn_copy);
+	palloc_free_page (cmd_line_copy);
   return tid;
 }
 
@@ -445,7 +454,7 @@ setup_stack (void **esp)
 	{
 	  success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
 	  if (success)
-		*esp = PHYS_BASE;
+		*esp = PHYS_BASE - 12;
 	  else
 		palloc_free_page (kpage);
 	}
