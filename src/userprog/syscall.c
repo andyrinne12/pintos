76a164fbf24addfe854831a0418a32f3889db7af
Vlad <vladnicolaescucollege@gmail.com>
2020-11-10 14:15:03 +0200

    [vn119] syscall_handler() basic implementation. Decode sys_call_number and introduce all syscall cases

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -60,7 +60,7 @@ static uint32_t load_memory_address(void *vaddr){
 // -----------------------------------------------------------------
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
@@ -68,6 +68,52 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  uint32_t sys_call_number = load_memory_address(f->esp);
+  switch (sys_call_number)
+	{
+	  /* Halt the operating system. */
+	  case SYS_HALT:
+		break;
+	  /* Terminate this process. */
+	  case SYS_EXIT:
+		break;
+	  /* Start another process. */
+	  case SYS_EXEC:
+		break;
+	  /* Wait for a child process to die. */
+	  case SYS_WAIT:
+		break;
+	  /* Create a file. */
+	  case SYS_CREATE:
+		break;
+	  /* Delete a file. */
+	  case SYS_REMOVE:
+		break;
+	  /* Open a file. */
+	  case SYS_OPEN:
+		break;
+	  /* Obtain a file's size. */
+	  case SYS_FILESIZE:
+		break;
+	  /* Read from a file. */
+	  case SYS_READ:
+		break;
+	  /* Write to a file. */
+	  case SYS_WRITE:
+		break;
+	  /* Change position in a file. */
+	  case SYS_SEEK:
+		break;
+	  /* Report current position in a file. */
+	  case SYS_TELL:
+		break;
+	  /* Close a file. */
+	  case SYS_CLOSE:
+		break;
+	  /* Handle default case. Unrecognized system call. */
+	  default:
+		break;
+	}
+//  printf ("system call!\n");
+//  thread_exit ();
 }
